29eaacb2cd97298eff04c80adb79ff18f63722d0
halyssonmarques <halyssonmarques@yahoo.com.br>
2022-03-25 23:06:31 -0300

    Criados atributo completo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(selfn nome= None, idade=35):
+    def __init__(self, *filhos, nome= None, idade=35):
         self.idade = idade
-        self.nome = None
+        self.nome = nome
+        self.filhos = list (filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__== '__main__':
-     p = Pessoa()
-     print(Pessoa.cumprimentar(p))
-     print(id(p))
-     print(p.cumprimentar())
-     print(p.nome)
-     p.nome = 'python'
-     print(p.nome)
-     print(p.idade)
+    python = Pessoa(nome='python')
+     lucicano = Pessoa(python, nome='luciano')
+     print(Pessoa.cumprimentar(luciano))
+     print(id(luciano))
+     print(luciano.cumprimentar())
+     print(luciano.nome)
+     print(luciano.idade)
+     for filhos in luciano.filhos:
+         print(filhos.nome)
